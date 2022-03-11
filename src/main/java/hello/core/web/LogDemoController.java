4bf32267e64460b78223090d09ea652ece22ad2e
FloralLife <ocard9611@naver.com>
2022-03-12 00:10:47 +0900

    scope proxyMode 로 객체 조회 지연 처리하여 request 스코프를 싱글톤처럼 사용

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,14 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger = " + myLogger.getClass());
         String requestURL = request.getRequestURL().toString();
-        myLogger.setRequestUrl(requestURL);
+        myLogger.setRequestUrl(requestURL); 
 
         myLogger.log("controller test");
         logDemoService.logic("testID");
